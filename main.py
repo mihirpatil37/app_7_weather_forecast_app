b6cdd99805cb54505ec7f04a68e8ca58714493ef
Mihir Patil <patilmihir9588@gmail.com>
2024-12-29 13:51:26 +0100

    Day 32: Created Backend, Filtered and extracted data from weather api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_weather_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -12,13 +12,9 @@ option = st.selectbox("Select data to view",("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place} ")
 
-def get_data(days):
-    dates=["2022-12-07", "2022-12-08", "2022-12-09", "2022-12-10", "2022-12-11"]
-    temperatures=[20, 21, 12, 13, 24]
-    temperatures=[days * i for i in temperatures]
-    return dates, temperatures
 
-d, t = get_data(days)
+
+d, t = get_weather_data(place, days, option)
 
 figure= px.line(x=d,y= t, labels={"x":"Date", "y":"Temperature(C)"})
 st.plotly_chart(figure)
