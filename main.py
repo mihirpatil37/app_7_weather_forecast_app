b300f3611386da04dc23ce2556e144064aeba338
Mihir Patil <patilmihir9588@gmail.com>
2024-12-29 12:13:56 +0100

    Day 32: plot fake data dynamically.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -9,3 +11,16 @@ days = st.slider("Forecast Days: ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place} ")
+
+def get_data(days):
+    dates=["2022-12-07", "2022-12-08", "2022-12-09", "2022-12-10", "2022-12-11"]
+    temperatures=[20, 21, 12, 13, 24]
+    temperatures=[days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure= px.line(x=d,y= t, labels={"x":"Date", "y":"Temperature(C)"})
+st.plotly_chart(figure)
+
+
